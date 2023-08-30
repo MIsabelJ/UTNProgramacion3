2299440e131fa7aa2026549fb140df3bef70e0c8
MIsabelJ <maria.jurman@alumnos.frm.utn.edu.ar>
2023-08-29 21:02:28 -0300

    Agrego los proyectos creados para los ejercicios 1 y 2

---
+++ Spring Ejercicios/Ej01/ejer01/src/main/java/com/utn/ejer01/repositories/PersonaRepository.java
@@ -0,0 +1,9 @@
+package com.utn.ejer01.repositories;
+
+import com.utn.ejer01.entities.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonaRepository extends JpaRepository<Persona, Long> {
+}
---
+++ Spring Ejercicios/Ej02/ejer02/src/main/java/com/utn/ejer02/entities/Persona.java
@@ -0,0 +1,30 @@
+package com.utn.ejer02.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Persona implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    // RELACION ONE TO ONE UNIDIRECCIONAL
+    // El Cascadeo propaga las operaciones y orphanRemoval asegura que se elimine la relacionada
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "domicilio_id")
+    private Domicilio domicilio;
+}
